ca8e3958aec78b4926bd9bd424bc452280c7f25f
Pavel I. Kryukov <kryukov@frtk.ru>
2020-03-29 20:58:48 +0300

    Support NEF files (RAW images)

--- phototools.py
+++ phototools.py
@@ -24,14 +24,18 @@ import datetime
 import glob
 import hashlib
 import imagehash
-import PIL.Image
 import os
+import PIL.Image
 import shutil
 
 # Returns global paths to all *.jpg files in directory
 def get_jpegs(path):
     return glob.iglob(path + '/**/*.jpg', recursive=True)
-    
+
+# Returns global paths to all *.nef files in directory
+def get_nefs(path):
+    return glob.iglob(path + '/**/*.nef', recursive=True)
+
 # Converts data to paths
 def date_to_path(data):
     return datetime.datetime.strptime(data, '%Y:%m:%d %H:%M:%S').strftime('%Y/%m %B')
@@ -48,9 +52,21 @@ def get_exif_date(pic):
     exif_data = img._getexif()
     return exif_data.get(36867)
 
+# Returns NEF datestamp
+# Taken from
+# https://pascalbrokmeier.de/2018-01-24/2018-01-24-Getting_the_capture_date_in_a_nef_Nikon_raw_file_with_python/
+def get_nef_date(pic):
+    raw_chunk = open(pic, 'rb')
+    raw_chunk.seek(2964)
+    capture_date_bin = raw_chunk.read(19)
+    return str(capture_date_bin)[2:-1]
+
+def get_date(pic):
+    return get_nef_date(pic) if pic.lower().endswith('.nef') else get_exif_date(pic)
+
 # Returns destination filename
 def get_new_name(pic, dst_path):
-    date = get_exif_date(pic)
+    date = get_date(pic)
     if date is not None:
         return "{}/{}/{}".format(dst_path, date_to_path(date), os.path.basename(pic))
 
@@ -60,7 +76,7 @@ def sha256sum(filename):
         for b in iter(lambda : f.read(128*1024), b''):
             h.update(b)
     return h.hexdigest()
-    
+
 # Move a single file
 def move_file(src, dst_path):
     dst = get_new_name(src, dst_path)
@@ -71,7 +87,7 @@ def move_file(src, dst_path):
         shutil.move(src, dst)
                
 # Checks if directory has duplicate files
-# Compares only EXIF + SHA, ignores content
+# Compares only datestamp and SHA, ignores content
 # (i.e. ignores photoshoot series)
 def check_duplicates(path):
     results = dict()
@@ -80,13 +96,13 @@ def check_duplicates(path):
         import PIL.Image
         if (n % 100) == 0:
             print ('photos loaded: ' + str(n))
-        d = get_exif_data(pic)
+        d = get_data(pic)
         if results.get(d) is not None:
             if sha256sum(pic) == sha256sum(results.get(d)):
                 print (pic + ' and ' + results.get(d) + ' are same files')
         results[d] = pic
         n += 1
-        
+
 # Returns imagehash (see https://pypi.org/project/ImageHash/)
 def get_imagehash(pic):
     try:
@@ -128,4 +144,6 @@ def move_all_takes(src_path, dst_path):
 # If file exists already, it is skipped
 def move_all_files(src_path, dst_path):
     for pic in get_jpegs(src_path):
+        move_file(pic, dst_path)
+    for pic in get_nefs(src_path):
         move_file(pic, dst_path)
